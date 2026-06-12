1f34b1bd6065ea35af34667d31574ea2a7161140
Yakumo-CU <z9t2224tzj@privaterelay.appleid.com>
2026-06-12 20:46:45 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhHUallQGv82QyrYTwUUo4113Swb7ZjBg",
+  authDomain: "ideaup-957dc.firebaseapp.com",
+  projectId: "ideaup-957dc",
+  storageBucket: "ideaup-957dc.firebasestorage.app",
+  messagingSenderId: "257938876169",
+  appId: "1:257938876169:web:158f182a798675802c95d8",
+  measurementId: "G-6WP7BD7BC7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
